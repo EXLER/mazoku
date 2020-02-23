e46b3b7f1941e4b57f5ec6c92ff36503fe588925
Kamil Marut <kakkmar@gmail.com>
2020-02-23 12:22:12 +0100

    Add maze generation algorithm

---
+++ maze_generator.py
@@ -0,0 +1,148 @@
+import random
+
+# Cardinal directions representation
+N, S, W, E = ('n', 's', 'w', 'e')
+
+class Cell:
+    """
+    Cell class containing its position and still standing neighboring walls.
+    """
+    def __init__(self, x, y, walls):
+        self.x = x
+        self.y = y
+        self.walls = set(walls)
+
+        self.target = False
+
+    def __repr__(self):
+        return f"Cell <{self.x}, {self.y}, {''.join(sorted(self.walls))}>"
+
+    def __contains__(self, wall):
+        return wall in self.walls
+
+    def _wall_to(self, other):
+        """
+        Returns the direction to the given adjacent cell from the current one.
+        """
+        assert abs(self.x - other.x) + abs(self.y - other.y) == 1, f"{self}, {other}"
+
+        if other.y < self.y:
+            return N
+        elif other.y > self.y:
+            return S
+        elif other.x < self.x:
+            return W
+        elif other.x > self.x:
+            return E
+
+    def connect(self, other):
+        """
+        Removes the wall between two adjacent cells.
+        """
+        self.walls.remove(self._wall_to(other))
+        other.walls.remove(other._wall_to(self))
+
+    def is_full(self):
+        """
+        Returns True if all walls are still standing.
+        """
+        return len(self.walls) == 4
+
+class Maze:
+    """
+    Maze class containing maze generation algorithm.
+    """
+    def __init__(self, width=16, height=16):
+        """
+        Create a new maze of given size.
+        """
+        self.width = width
+        self.height = height
+        self.cells = []
+
+        for y in range(self.height):
+            for x in range(self.width):
+                self.cells.append(Cell(x, y, [N, S, W, E]))
+
+    def __getitem__(self, coords):
+        """
+        Returns the cell at coords = (x, y).
+        """
+        x, y = coords
+
+        if 0 <= x < self.width and 0 <= y < self.height:
+            # The cell is inside the maze
+            return self.cells[x + y * self.width]
+        else:
+            return None
+
+    def neighbors(self, cell):
+        """
+        Returns the list of neighboring cells (without diagonals).
+        Cells on borders or corners may have less than 4 neighbors.
+        """
+        x = cell.x
+        y = cell.y
+        for nx, ny in [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]:
+            neighbor = self[nx, ny]
+            if neighbor is not None:
+                yield neighbor
+
+    def randomize(self):
+        """
+        Depth-First Search maze generation algorithm.
+
+        Knocks down random walls to build a random maze.
+        """
+        cell_stack = []
+        cell = random.choice(self.cells)
+        n_visited_cells = 1
+
+        while n_visited_cells < len(self.cells):
+            neighbors = [c for c in self.neighbors(cell) if c.is_full()]
+
+            if len(neighbors):
+                neighbor = random.choice(neighbors)
+                cell.connect(neighbor)
+                cell_stack.append(cell)
+                cell = neighbor
+                n_visited_cells += 1
+            else:
+                cell = cell_stack.pop()
+
+        random.choice(self.cells).target = True
+
+
+    def to_string(self):
+        """
+        Returns a string representation of the maze matrix.
+        """
+        str_matrix = [['#'] * (self.width * 2 + 1) for i in range(self.height * 2 + 1)]
+
+        for cell in self.cells:
+            x = cell.x * 2 + 1
+            y = cell.y * 2 + 1
+            str_matrix[y][x] = '.'
+            if N not in cell and y > 0:
+                str_matrix[y - 1][x] = '.'
+            if S not in cell and y + 1 < self.width:
+                str_matrix[y + 1][x] = '.'
+            if W not in cell and x > 0:
+                str_matrix[y][x - 1] = '.'
+            if E not in cell and x + 1 < self.width:
+                str_matrix[y][x + 1] = '.'
+            if cell.target:
+                str_matrix[y][x] = '%'
+
+        return '\n'.join(''.join(line) for line in str_matrix)
+
+
+    @staticmethod
+    def generate(width=16, height=16):
+        """
+        Returns a random maze of given size.
+        """
+        m = Maze(width, height)
+        m.randomize()
+        return m.to_string()
+
--- mazoku.py
+++ mazoku.py
@@ -1,5 +1,7 @@
 import math
 
+from maze_generator import Maze
+
 try:
     import pyxel
 except ImportError:
@@ -11,49 +13,33 @@ SCREEN_HEIGHT = 128
 
 MAP_WIDTH = 16
 MAP_HEIGHT = 16
-
-TEMP_MAP = """
-################
-#..............#
-#..#....######.#
-#..............#
-#..............#
-#......##......#
-#......##......#
-#..............#
-###....#####...#
-##.............#
-#..............#
-#........###.###
-#........#.....#
-#..............#
-#........#.....#
-################
-"""
+GENERATOR_MAP_WIDTH = int(MAP_WIDTH / 2 - 1)
+GENERATOR_MAP_HEIGHT = int(MAP_HEIGHT / 2 - 1)
 
 class Game:
-    def __init__(self):
+    def __init__(self, map):
+        self.maze_map = map.split('\n')
+
         pyxel.init(
             SCREEN_WIDTH,
             SCREEN_HEIGHT,
             caption="Mazoku",
             scale=4
         )
-        # pyxel.load("mazoku.pyxel")
 
-        self.maze_map = TEMP_MAP[1:-1].split("\n")
         self.tp1 = pyxel.frame_count
         self.tp2 = pyxel.frame_count
         self.depth = 24
         self.render_alt = True
         self.show_boundaries = True
-        self.show_minimap = True
+        self.show_minimap = False
 
-        self.player_x = 7
-        self.player_y = 12
-        self.player_z = 4
+        self.player_x = 3
+        self.player_y = 3
+        self.player_z = 1
         self.fov = math.pi / 4
         self.speed = 0.2 # Movement speed
+        self.complete = False
 
     def run(self):
         pyxel.run(self.update, self.draw)
@@ -63,147 +49,179 @@ class Game:
         delta_time = self.tp2 - self.tp1
         self.tp1 = self.tp2
 
-        if pyxel.btn(pyxel.KEY_W):
-            self.move_forward(delta_time)
+        if not self.complete:
+            if pyxel.btn(pyxel.KEY_W):
+                self.move_forward(delta_time)
 
-        if pyxel.btn(pyxel.KEY_S):
-            self.move_backward(delta_time)
+            if pyxel.btn(pyxel.KEY_S):
+                self.move_backward(delta_time)
 
-        if pyxel.btn(pyxel.KEY_A):
-            self.player_z -= 0.05 * delta_time
+            if pyxel.btn(pyxel.KEY_A):
+                self.player_z -= 0.05 * delta_time
 
-        if pyxel.btn(pyxel.KEY_D):
-            self.player_z += 0.05 * delta_time
+            if pyxel.btn(pyxel.KEY_D):
+                self.player_z += 0.05 * delta_time
 
-        if pyxel.btnp(pyxel.KEY_P):
-            print(f"x: {self.player_x}")
-            print(f"y: {self.player_y}")
-            print(f"z: {self.player_z}")
+        if pyxel.btn(pyxel.KEY_R):
+            self.maze_map = Maze.generate(GENERATOR_MAP_WIDTH, GENERATOR_MAP_HEIGHT).split('\n')
+            self.complete = False
+
+        if pyxel.btn(pyxel.KEY_M):
+            self.show_minimap = not self.show_minimap
 
     def draw(self):
-        start = 0
-        if self.render_alt:
-            start = 1
-        self.render_alt = not self.render_alt
+        if not self.complete:
+            start = 0
+            if self.render_alt:
+                start = 1
+            self.render_alt = not self.render_alt
 
-        for x in range(start, SCREEN_WIDTH, 2):
-            # For each column, calculate the ray angle projected
-            ray_angle = (self.player_z - self.fov / 2) + (x / SCREEN_WIDTH) * self.fov
+            for x in range(start, SCREEN_WIDTH, 2):
+                # For each column, calculate the ray angle projected
+                ray_angle = (self.player_z - self.fov / 2) + (x / SCREEN_WIDTH) * self.fov
 
-            ray_distance = 0
-            ray_hit_wall = False
-            ray_boundary = False
+                ray_distance = 0
+                ray_hit_wall = False
+                ray_hit_exit = False
+                ray_boundary = False
 
-            view_x = math.sin(ray_angle)
-            view_y = math.cos(ray_angle)
+                view_x = math.sin(ray_angle)
+                view_y = math.cos(ray_angle)
 
-            while not ray_hit_wall and ray_distance < self.depth:
-                ray_distance += 0.1
+                while not ray_hit_wall and ray_distance < self.depth:
+                    ray_distance += 0.1
 
-                # Check if ray is out of map bounds
-                test_x = int(self.player_x + view_x * ray_distance)
-                test_y = int(self.player_y + view_y * ray_distance)
+                    # Check if ray is out of map bounds
+                    test_x = int(self.player_x + view_x * ray_distance)
+                    test_y = int(self.player_y + view_y * ray_distance)
 
-                if test_x < 0 or test_x >= MAP_WIDTH or test_y < 0 or test_y >= MAP_HEIGHT:
-                    ray_hit_wall = True
-                    ray_distance = self.depth
-                else:
-                    # Ray is still inside map space, check if the ray cell is a wall
-                    if self.maze_map[test_y][test_x] == "#":
+                    if test_x < 0 or test_x >= MAP_WIDTH or test_y < 0 or test_y >= MAP_HEIGHT:
                         ray_hit_wall = True
-
-                        # Cast rays from each wall corner to find boundaries
-                        wall_rays = []
-                        for wx in range(2):
-                            for wy in range(2):
-                                vx = test_x + wx - self.player_x
-                                vy = test_y + wy - self.player_y
-                                d = math.sqrt(vx**2 + vy**2)
-                                dot = (view_x * vx / d) + (view_y * vy / d)
-                                wall_rays.append((d, dot))
-
-                        # Sort to find the closest
-                        wall_rays.sort()
-
-                        # Look for small angles with closest corners
-                        bound = 0.01
-                        if math.acos(wall_rays[0][1]) < bound or math.acos(wall_rays[1][1]) < bound:
-                            ray_boundary = True
-                                
-            # Calculate distances to ceiling and floor
-            ceiling = SCREEN_HEIGHT / 2 - SCREEN_HEIGHT / ray_distance
-            floor = SCREEN_HEIGHT - ceiling
-
-            # Using Pyxel image bank as display buffer
-            shade = 0
-            for y in range(SCREEN_HEIGHT):
-                if y < ceiling:
-                    pyxel.image(1).set(x, y, 0)
-                elif y > ceiling and y <= floor:
-                    # Compute wall shading
-                    if ray_distance <= self.depth / 7:
-                        shade = 6
-                    elif ray_distance < self.depth / 6:
-                        if x % 2:
-                            shade = 6 if y % 2 else 13
-                        else:
-                            shade = 13 if y % 2 else 6
-                    elif ray_distance < self.depth / 5:
-                        shade = 13
-                    elif ray_distance < self.depth / 4:
-                        if x % 2:
-                            shade = 13 if y % 2 else 1
-                        else:
-                            shade = 1 if y % 2 else 13
-                    elif ray_distance < self.depth / 3:
-                        shade = 1
-                    elif ray_distance < self.depth / 2:
-                        if x % 2:
-                            shade = 1 if y % 2 else 0
-                        else:
-                            shade = 0 if y % 2 else 1
-                    elif ray_distance < self.depth:
-                        shade = 0
-
-                    # Change the shade of boundary between walls
-                    if self.show_boundaries and ray_boundary:
-                        shade = 0
-
-                    pyxel.image(1).set(x, y, shade)
-                else:
-                    # Compute floor shading
-                    b = 1 - (y - SCREEN_HEIGHT / 2) / (SCREEN_HEIGHT / 2)
-                    if b < 0.25:
-                        shade = 14
-                    elif b < 0.5:
-                        if x % 2:
-                            shade = 14 if y % 2 else 2
-                        else:
-                            shade = 2 if y % 2 else 14
-                    elif b < 0.75:
-                        shade = 2
-                    elif b < 0.9:
-                        if x % 2:
-                            shade = 2 if y % 2 else 0
-                        else:
-                            shade = 0 if y % 2 else 2
+                        ray_distance = self.depth
                     else:
-                        shade = 0
+                        # Ray is still inside map space, check if the ray cell is a wall
+                        if self.maze_map[test_y][test_x] == '#' or self.maze_map[test_y][test_x] == '%':
+                            ray_hit_wall = True
 
-                    pyxel.image(1).set(x, y, shade)
+                            if self.maze_map[test_y][test_x] == '%':
+                                ray_hit_exit = True
 
-        # Copy the screen from the image bank
-        pyxel.blt(0, 0, 1, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+                            # Cast rays from each wall corner to find boundaries
+                            wall_rays = []
+                            for wx in range(2):
+                                for wy in range(2):
+                                    vx = test_x + wx - self.player_x
+                                    vy = test_y + wy - self.player_y
+                                    d = math.sqrt(vx**2 + vy**2)
+                                    dot = (view_x * vx / d) + (view_y * vy / d)
+                                    wall_rays.append((d, dot))
+
+                            # Sort to find the closest
+                            wall_rays.sort()
+
+                            # Look for small angles with closest corners
+                            bound = 0.01
+                            if math.acos(wall_rays[0][1]) < bound or math.acos(wall_rays[1][1]) < bound:
+                                ray_boundary = True
+                                    
+                # Calculate distances to ceiling and floor
+                ceiling = SCREEN_HEIGHT / 2 - SCREEN_HEIGHT / ray_distance
+                floor = SCREEN_HEIGHT - ceiling
+
+                # Using Pyxel image bank as display buffer
+                shade = 0
+                for y in range(SCREEN_HEIGHT):
+                    if y < ceiling:
+                        pyxel.image(1).set(x, y, 0)
+                    elif y > ceiling and y <= floor:
+                        # Compute wall shading
+                        if ray_distance <= self.depth / 7:
+                            if ray_hit_exit:
+                                shade = 11
+                            else:
+                                shade = 6
+                        elif ray_distance < self.depth / 6:
+                            if ray_hit_exit:
+                                shade = 11
+                            else:
+                                if x % 2:
+                                    shade = 6 if y % 2 else 13
+                                else:
+                                    shade = 13 if y % 2 else 6
+                        elif ray_distance < self.depth / 5:
+                            if ray_hit_exit:
+                                shade = 11
+                            else:
+                                shade = 13
+                        elif ray_distance < self.depth / 4:
+                            if ray_hit_exit:
+                                shade = 4
+                            else:
+                                if x % 2:
+                                    shade = 13 if y % 2 else 1
+                                else:
+                                    shade = 1 if y % 2 else 13
+                        elif ray_distance < self.depth / 3:
+                            if ray_hit_exit:
+                                shade = 3
+                            else:
+                                shade = 1
+                        elif ray_distance < self.depth / 2:
+                            if ray_hit_exit:
+                                shade = 5
+                            else:
+                                if x % 2:
+                                    shade = 1 if y % 2 else 0
+                                else:
+                                    shade = 0 if y % 2 else 1
+                        elif ray_distance < self.depth:
+                            shade = 0
+
+                        # Change the shade of boundary between walls
+                        if self.show_boundaries and ray_boundary:
+                            shade = 0
+
+                        pyxel.image(1).set(x, y, shade)
+                    else:
+                        # Compute floor shading
+                        b = 1 - (y - SCREEN_HEIGHT / 2) / (SCREEN_HEIGHT / 2)
+                        if b < 0.25:
+                            shade = 14
+                        elif b < 0.5:
+                            if x % 2:
+                                shade = 14 if y % 2 else 2
+                            else:
+                                shade = 2 if y % 2 else 14
+                        elif b < 0.75:
+                            shade = 2
+                        elif b < 0.9:
+                            if x % 2:
+                                shade = 2 if y % 2 else 0
+                            else:
+                                shade = 0 if y % 2 else 2
+                        else:
+                            shade = 0
+
+                        pyxel.image(1).set(x, y, shade)
+
+            # Copy the screen from the image bank
+            pyxel.blt(0, 0, 1, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+        else:
+            pyxel.cls(0)
+            pyxel.text(SCREEN_WIDTH / 2 - 32, SCREEN_HEIGHT / 2 - 32, "Maze completed!", 15)
+            pyxel.text(SCREEN_WIDTH / 2 - 58, SCREEN_HEIGHT / 2 - 16, "Press R to generate a new maze", 15)
 
         # Draw minimap
         if self.show_minimap:
             startx = 0
             starty = SCREEN_HEIGHT - MAP_WIDTH * 2
-            for nx in range(MAP_HEIGHT):
-                for ny in range(MAP_WIDTH):
+            for nx in range(MAP_HEIGHT - 1):
+                for ny in range(MAP_WIDTH - 1):
                     cell = self.maze_map[ny][nx]
-                    if cell == "#":
+                    if cell == '#':
                         col = 6
+                    elif cell == '%':
+                        col = 11
                     else:
                         col = 7
                     pyxel.rect(
@@ -224,22 +242,32 @@ class Game:
         new_x = self.player_x + math.sin(self.player_z) * self.speed * delta_time
         new_y = self.player_y + math.cos(self.player_z) * self.speed * delta_time
         
-        if not self.collides(new_x, new_y):
-            self.player_x = new_x
-            self.player_y = new_y
+        if self.entered_exit(new_x, new_y):
+            self.complete = True
+        else:
+            if not self.collides(new_x, new_y):
+                self.player_x = new_x
+                self.player_y = new_y
 
     def move_backward(self, delta_time):
         new_x = self.player_x - math.sin(self.player_z) * self.speed * delta_time
         new_y = self.player_y - math.cos(self.player_z) * self.speed * delta_time
         
-        if not self.collides(new_x, new_y):
-            self.player_x = new_x
-            self.player_y = new_y
+        if self.entered_exit(new_x, new_y):
+            self.complete = True
+        else:
+            if not self.collides(new_x, new_y):
+                self.player_x = new_x
+                self.player_y = new_y
 
     def collides(self, x, y):
-        return self.maze_map[int(y)][int(x)] == "#"
+        return self.maze_map[int(y)][int(x)] == '#'
+
+    def entered_exit(self, x, y):
+        return self.maze_map[int(y)][int(x)] == '%'
+
 
 
 if __name__ == "__main__":
-    game = Game()
+    game = Game(Maze.generate(GENERATOR_MAP_WIDTH, GENERATOR_MAP_HEIGHT))
     game.run()
